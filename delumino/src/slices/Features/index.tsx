e7a4d6c086748cb19e170d111d66a43ab24e67a8
Calin <cotuna.calin@yahoo.com>
2025-07-07 15:13:31 +0300

    Reverted changes

--- delumino/src/slices/Features/index.tsx
+++ delumino/src/slices/Features/index.tsx
@@ -17,7 +17,7 @@ const Features = ({ slice }: FeaturesProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="max-w-6xl mx-auto p-4 pb-20"
+      className="max-w-6xl mx-auto p-4 pb-20 "
     >
       <h2 className="text-3xl font-bold text-center mb-6">
         <PrismicText field={slice.primary.title} />
